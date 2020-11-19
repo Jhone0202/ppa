3187668a1a61688949b78ffab9bf0c057ff1fdfb
Jhone0202 <maicon.jhone100@gmail.com>
2020-11-19 16:28:36 -0300

    Blog with Next.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,54 @@
-function Home() {
-    return <div>My Homepage</div>
+import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/date'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+
+export default function Home({ allPostsData }) {
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+
+            <section className={utilStyles.headingMd}>
+                <p>
+                    Hi, I am Maicon, and this blog has a way to learn React and Next.js. You can create a website like this by following the {' '}
+                    <a href="https://nextjs.org/learn"> tutorial Next.js</a>.
+                </p>
+
+            </section>
+
+            <section className={utilStyles.headingMd}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+
+                            <br />
+
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
+
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
+    )
 }
 
-export default Home
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
